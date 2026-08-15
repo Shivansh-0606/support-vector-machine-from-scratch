66ccfb5d4aac07dbf2055d4a0b0b541b730ccaf2
Shivansh Jhalani <shivanshjhalani123@gmail.com>
2026-08-15 20:14:13 +0530

    Step 9: train_svm

--- model.py
+++ model.py
@@ -122,8 +122,18 @@ def apply_update(params, grads, learning_rate):
         'b':new_b
     }
 
-# Step 9 - train_svm (not yet solved)
-# TODO: implement
+# Step 9 - train_svm
+def train_svm(x, y, learning_rate, reg_lambda, n_epochs):
+    # TODO: fit a linear SVM by repeatedly updating parameters over n_epochs passes.
+
+    params =  initialize_parameters(x.shape[1])
+
+    for epoch in range(n_epochs):
+        grads =  compute_gradients(x, y, params, reg_lambda)
+
+        params = apply_update(params , grads , learning_rate)
+    
+    return params
 
 # Step 10 - predict_labels (not yet solved)
 # TODO: implement
